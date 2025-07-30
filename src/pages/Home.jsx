8e83db3b1e3046a1a43dd0013f34556b19e9305a
Jamie <jamie.isaacs@sanlam.co.za>
2025-07-30 18:33:02 +0200

    Improved styling on Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
+import { Typography } from "@mui/material";
+
 export function Home() {
   return (
     <div className="banner">
       <div className="banner-text">
-        <h1 className="banner-title">
+        <Typography variant="h3" className="banner-title">
           Welcome to TechProtect: Your device insurance provider
-        </h1>
-        <p className="banner-subtitle">
+        </Typography>
+        <Typography variant="h5" className="banner-subtitle">
           Protect your tech with flexible plans and hassle-free claims.
-        </p>
+        </Typography>
       </div>
     </div>
   );
